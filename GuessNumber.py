5df57704f3c1858375255cf3f00f7aba32e59f0e
Jocelyn Brockman <ymeworld@yahoo.com>
2015-06-08 03:02:03 -0700

    Update GuessNumber.py

--- GuessNumber.py
+++ GuessNumber.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
 
+"""/**
+ ThinkFul Computer Information Systems
+ @AUTHOR: Jocelyn M.
+ Thinkful Spring 2015
+ Programming in Python Basics 
+ API Guessing a number game
+ *****************************************************************************************
+ Execution:  ./GuessNumber.py
+ WHAT: This program promps the user to guss a number between 0 and 100 withing a 5 tries. 
+ 
+ *****************************************************************************************/ """
+
 import random
 
 number = random.randint(0, 100)
-tries = 1
+tries  = 1
 
 guess = int(input("Enter your guess: "))
 
