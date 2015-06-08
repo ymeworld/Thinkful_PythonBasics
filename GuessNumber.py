d239dca0fdddd9d0add946b0f0727f51c3fbde4b
Jocelyn Broackman <ymeworld@yahoo.com>
2015-06-07 23:51:40 -0700

    First Commit

---
+++ GuessNumber.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import random
+
+number = random.randint(0, 100)
+  = 0
+
+guess = int(input("Enter your guess: "))
+
+while guess != number and tries < 5:
+    if guess > number:
+        print("Too High")
+    else:
+        print("Too Low")
+
+    tries += 1
+    guess = int(input("Enter your guess: "))
+
+if guess != number:
+    print("You lost, the number is: ", number)
+else:
+    print("You got it right!. The number was: ", number)
